b7dc3da775f8b49ed78c3e657ab70b684dd795cc
Thiago Costa <tjthiagocosta@gmail.com>
2022-02-06 01:12:10 -0300

    Aula comentários em JS

---
+++ aula4/comentarios.js
@@ -0,0 +1,14 @@
+//Todos os comentários serão ignorados no terminal.
+console.log("Linha 1")
+//Comentario 2
+console.log("Linha 2")
+//Comentário 3
+console.log("Linha 3")
+//comentário 4
+console.log("Linha 4")
+
+/**
+ * comentário
+ * de varias
+ * linhas
+ */
