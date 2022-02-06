e3d71f8dcfb845e137333ca1c08ff72e182fd468
Thiago Costa <tjthiagocosta@gmail.com>
2022-02-06 12:27:47 -0300

    Declaração de variáveis com let

---
+++ aula6/index.js
@@ -0,0 +1,8 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não podemos começar o nome de uma variável com números
+// O nome não pode conter espaços ou traços
+// Por convenção, devemos utilizar camelCase
+// Devemos utilizar "let" e não "var"
+let nomeCompletoDoCliente = "João da Silva";
+console.log(nomeCompletoDoCliente, "nasceu em 1995")
